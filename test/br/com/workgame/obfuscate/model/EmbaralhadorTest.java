8855acb6f3eb15ba19aed7e76361612f933ed122
jonathan Scheibel de Morais <jonathansmorais@gmail.com>
2016-09-23 20:15:54 -0300

    Adicionada unit de teste para embaralhadores

---
+++ test/br/com/workgame/obfuscate/model/EmbaralhadorTest.java
@@ -0,0 +1,14 @@
+package br.com.workgame.obfuscate.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmbaralhadorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
