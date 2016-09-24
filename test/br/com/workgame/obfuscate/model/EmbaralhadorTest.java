f1e62983a24260ac7763b3cd243cd60aa8f128f0
jonathan Scheibel de Morais <jonathansmorais@gmail.com>
2016-09-23 21:24:30 -0300

    Criação do diagrama de classe das classes criadas

--- test/br/com/workgame/obfuscate/model/EmbaralhadorTest.java
+++ test/br/com/workgame/obfuscate/model/EmbaralhadorTest.java
@@ -4,11 +4,45 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import br.com.workgame.obfuscate.interfaces.Embaralhador;
+
 public class EmbaralhadorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmbaralhadorAnalogico() {
+		Embaralhador analogico = new EmbaralhadorAnalogico();
+		try {
+			String original = "Lorem Ipsum";
+			String altered = analogico.embaralhar(original);
+			assertFalse(altered.equals(original));
+		} finally {
+			analogico = null;
+		}
 	}
 
+	
+	@Test
+	public void testEmbaralhadorDigital() {		
+		Embaralhador digital = new EmbaralhadorDigital();
+		try {
+			String original = "Lorem Ipsum";
+			String altered = digital.embaralhar(original);
+			assertFalse(altered.equals(original));
+		} finally {
+			digital = null;
+		}
+	}
+	
+	
+	@Test
+	public void testEmbaralhadorReverso() {
+		Embaralhador reverso = new EmbaralhadorReverso();
+		try {			
+			String original = "Lorem Ipsum";
+			String altered = reverso.embaralhar(original);
+			assertTrue(altered.equals("muspI meroL"));
+		} finally {
+			reverso = null;
+		}
+	}						  
 }
